8f43b8ef9aafd3d4dbc53be5feab273b2b6191a2
Aadarsh MK <75124918+AADARSH-MADAN-KOLLAN@users.noreply.github.com>
2021-12-08 19:57:08 +0530

    Update PRO-C97.py

--- PRO-C97.py
+++ PRO-C97.py
@@ -1,21 +1,19 @@
 import random
 
-print("✧･ﾟ: *✧･ﾟ:* WELCOME TO THE NUMBER GUESSING GAME! *:･ﾟ✧*:･ﾟ✧ \n")
+print("✧･ﾟ: *✧･ﾟ:* WELCOME TO THE NUMBER GUESSING GAME! *:･ﾟ✧*:･ﾟ✧\n")
 print("Guess a number from 1 to 10! You have 5 chances!\n")
-number = random.randrange(1,10)
-chance=0
+number = random.randrange(1, 10)
+chance = 5
 
-while chance<5:
-    guess = int(input("Your Guess : "))
+while chances != 0:
+    guess = int(input(f"Your guess: "))
     if guess == number:
         print("CONGRATULATIONS YOU WIN!!!!")
-        break
-    elif guess<number:
-        chance+=1
-        print(f"Sorry, your guess is wrong. You have {5-chance} chances left! HINT: It's a bigger number!")
-    elif guess>number:
-        chance+=1
-        print(f"Sorry, your guess is wrong. You have {5-chance} chances left! HINT: It's a smaller number!")
-    else:
-        chance+=1
-        print(f"Sorry, your guess is wrong. You have {5-chance} chances left!")
+        exit()
+    elif guess < number:
+        chance -= 1
+        print(f"Sorry, your guess is wrong. You have {chance} chances left! HINT: It's a bigger number!")
+    elif guess > number:
+        chance -= 1
+        print(f"Sorry, your guess is wrong. You have {chance} chances left! HINT: It's a smaller number!")
+print("Uh oh. You've run out of chances :(")
